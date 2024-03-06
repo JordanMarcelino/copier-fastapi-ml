55794db705e7ad921f225dc72a8b14418f4e603a
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-03-06 22:21:13 +0700

    ✨ Add user entity - ✨ Add auth endpoint

---
+++ development/app/api/routes/auth.py
@@ -0,0 +1,4 @@
+from fastapi.routing import APIRoute
+from fastapi.routing import HTTPException
+
+router = APIRoute()
---
+++ development/app/schemas/auth.py
@@ -0,0 +1,51 @@
+import enum
+from datetime import datetime
+from typing import Optional
+from uuid import uuid4
+
+from sqlmodel import BigInteger
+from sqlmodel import Enum
+from sqlmodel import Uuid
+from sqlmodel import Field
+from sqlmodel import SQLModel
+
+
+class Status(enum.Enum):
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+
+
+class Role(enum.Enum):
+    ADMIN = "admin"
+    MEMBER = "member"
+
+
+class UserBase(SQLModel):
+    email: str = Field(unique=True, index=True, nullable=False)
+
+
+class User(UserBase, table=True):
+    id: Uuid = Field(default=uuid4, primary_key=True, nullable=False)
+    password: str = Field(nullable=False)
+    status: Enum = Field(default=Status.ACTIVE, nullable=False)
+    role: Enum = Field(default=Role.MEMBER, nullable=False)
+    created_at: BigInteger = Field(
+        default=lambda _: int(datetime.now().timestamp()), nullabe=False
+    )
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserRead(UserBase):
+    id: str
+    status: Status
+    role: Role
+    created_at: int
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+    status: Optional[Status] = None
+    role: Optional[Role] = None
